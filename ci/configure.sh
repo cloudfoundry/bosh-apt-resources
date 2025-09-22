6b095a59a4b75a2f0d679daf426ca612197ab59c
ramonskie <ramon.makkelie@gmail.com>
2025-09-22 18:00:30 +0200

    corosopond to teams in ci

--- ci/configure.sh
+++ ci/configure.sh
@@ -4,8 +4,8 @@ set -e
 
 FLY="${FLY_CLI:-fly}"
 
-until "$FLY" -t "${CONCOURSE_TARGET:-bosh}" status;do
-  "$FLY" -t "${CONCOURSE_TARGET:-bosh}" login
+until "$FLY" -t "${CONCOURSE_TARGET:-main}" status;do
+  "$FLY" -t "${CONCOURSE_TARGET:-main}" login
   sleep 1
 done
 
@@ -13,6 +13,6 @@ pipeline_config=$(mktemp)
 ytt --dangerous-allow-all-symlink-destinations \
     -f "$(dirname $0)" > $pipeline_config
 
-"$FLY" -t "${CONCOURSE_TARGET:-bosh}" set-pipeline \
+"$FLY" -t "${CONCOURSE_TARGET:-main}" set-pipeline \
   -p "bosh-apt-resources" \
   -c "$pipeline_config"
